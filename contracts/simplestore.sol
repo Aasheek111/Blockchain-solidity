21b8fa8ab1b3c7fea9fffd2f0e823b262a35ed5a
Aasheek111 <technicalaashik111@gmail.com>
2026-05-03 08:29:36 +0545

    mapping and license

--- contracts/simplestore.sol
+++ contracts/simplestore.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract store{
@@ -15,11 +16,15 @@ contract store{
 
 // People public people =People({favnum: 10, name: "sajid"});
 
+mapping(string=>uint) public nameToFavNum;
+
 People[] public people;
 //memory for temporary and storage for permanent 
 //if we use string as a paramater then we have you use either memory or storage
 function addPerson(uint fav, string  memory naam )public {
     people.push(People(fav,naam));
+    //mapping 
+    nameToFavNum[naam]=fav;
 }
 function getdata() public view returns (People[] memory){
      return people;
